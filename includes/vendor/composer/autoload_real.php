721efd5fb89d62939de452338bbd5ee6f5442ae6
Surendhar SE <surendhar.elamurugan@sezzle.com>
2021-05-17 22:23:33 +0530

    updated vendor

--- includes/vendor/composer/autoload_real.php
+++ includes/vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit81ecffd7c951c4af2b8488df79d98b9a
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit81ecffd7c951c4af2b8488df79d98b9a', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit81ecffd7c951c4af2b8488df79d98b9a', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit81ecffd7c951c4af2b8488df79d98b9a::getInitializer($loader));
         } else {
